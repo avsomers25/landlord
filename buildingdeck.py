51b61b0e52687141ec5444e0af6ee19e23ad941b
Andrew Somers <avsomers25@gmail.com>
2023-03-03 13:44:27 -0500

    adding things
    adding things

--- buildingdeck.py
+++ buildingdeck.py
@@ -1,7 +1,13 @@
-base_values = {}
-with open("file.txt") as f:
-    for line in f:
-       (key, val) = line.split()
-       d[key] = int(val)
+import pandas as pd
 
-print(d)
+df = pd.read_csv ('base_val.csv', header = None, names =["Base_val"], index_col = 0)
+print(df)
+
+deck = {}
+deck["Apple"] = (1, df.loc["Apple", "Base_val"])
+
+if(len(deck) < 20):
+    deck["Empty"] = (20 - len(deck), df.loc["Empty", "Base_val"])
+
+    
+print(deck)
